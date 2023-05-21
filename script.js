d2ae6ca943222f43a1e2bd3641c786236d0133f7
Omer Bank <omerbank111@gmail.com>
2023-05-21 23:20:39 +0300

    Add function that 'fixes' the player selection, and function that plays out a single round

--- script.js
+++ script.js
@@ -4,10 +4,33 @@ function getRandomInt(max) {
 
 function getComputerChoice(num=getRandomInt(3)) {
     if (num === 0) {
-        return "rock";
+        return "Rock";
     } else if (num === 1) {
-        return "paper";
+        return "Paper";
     }
 
-    return "scissors";
+    return "Scissors";
+}
+
+function firstLetterUpper(str) {
+    const firstLetter = str[0].toUpperCase();
+    const strLowerCase = str.slice(1).toLowerCase();
+
+    return firstLetter + strLowerCase;
+}
+
+function playRound(playerSelection, computerSelection) {
+    const playerFixed = firstLetterUpper(playerSelection);
+    
+    if (playerFixed === computerSelection) { 
+        return "It's a tie!";
+    } else if (playerFixed === "Rock" && computerSelection === "Scissors") {
+        return "You win! Rock beats Scissors.";
+    } else if (playerFixed === "Paper" && computerSelection === "Rock") {
+        return "You win! Paper beats rock.";
+    } else if (playerFixed === "Scissors" && computerSelection === "Paper") {
+        return "You win! Scissors beats paper.";
+    }
+
+    return `You lose! ${computerSelection} beats ${playerFixed.toLowerCase()}.`;
 }
